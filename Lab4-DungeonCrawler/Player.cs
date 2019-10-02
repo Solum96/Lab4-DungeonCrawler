e8faebe142e8c38838ba8bcded4698f96e79211c
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-02 09:11:16 +0200

    Added Player movement and temporary(?) mapArray

--- Lab4-DungeonCrawler/Player.cs
+++ Lab4-DungeonCrawler/Player.cs
@@ -5,5 +5,29 @@
         public Player()
         {
         }
+
+        public static void MovePlayer(char[,] mapArray, int y, int x)
+        {
+            int rowIndex = 0;
+            int columnIndex = 0;
+            for (int i = 0; i < mapArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapArray.GetLength(1); j++)
+                {
+                    if (mapArray[i, j].Equals('@'))
+                    {
+                        rowIndex = i;
+                        columnIndex = j;
+                        break;
+                    }
+                }
+            }
+
+            if(mapArray[rowIndex + y, columnIndex + x] != '#')
+            {
+                mapArray[rowIndex, columnIndex] = '-';
+                mapArray[rowIndex + y, columnIndex + x] = '@';
+            }
+        }
     }
 }

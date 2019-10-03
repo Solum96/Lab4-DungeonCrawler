835b29c91a03e0c3adf7bf7fe24c55b40258f9cc
Alexander Molnar <Molex90@gmail.com>
2019-10-03 14:18:39 +0200

    Added Game Object class
    Added chars representing different objects.

---
+++ Lab4-DungeonCrawler/GameObject.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab4_DungeonCrawler
+{
+    partial class Program
+    {
+        public abstract class GameObject
+        {
+            public GameObject()
+            {
+
+            }
+
+            char Key = 'k';
+            char Door = 'D';
+            char Trap;
+            char Torch = 't';
+            char Exit = 'v';
+
+
+        }
+    }
+}

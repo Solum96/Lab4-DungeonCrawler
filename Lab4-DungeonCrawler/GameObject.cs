77cb51e89ee24d6da15b99f233fef110eb2c105c
Alexander Molnar <Molex90@gmail.com>
2019-10-03 14:36:44 +0200

    Auto stash before revert of "Added Game Object class"

---
+++ Lab4-DungeonCrawler/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab4_DungeonCrawler
+{
+    partial class Program
+    {
+        public abstract class GameObject
+        {
+            public GameObject()
+            {
+
+            }
+
+            static char[] ObjectRandomizer = new char[] { 'k', 't', 'b' };
+            static Random randomObject = new Random();
+            static int randomIndex = randomObject.Next(ObjectRandomizer.Length);
+
+            public char Key { get; set; } = 'k';
+            public char Door { get; set; } = 'D';
+            public char Bomb { get; set; } = 'b';
+            public char Trap { get; set; } = ObjectRandomizer[randomIndex];
+            public char Torch = 't';
+            public char Exit = 'v';
+
+        }
+    }
+}

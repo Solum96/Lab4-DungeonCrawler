e8be1574649b2c543feab3e5191938e1b548f4a5
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-09 16:14:40 +0200

    Set trap visual to '-'

--- Lab4-DungeonCrawler/GameObjects/Trap.cs
+++ Lab4-DungeonCrawler/GameObjects/Trap.cs
@@ -5,7 +5,7 @@ namespace Lab4_DungeonCrawler.GameObjects
     public class Trap : GameObject
     {
         public Trap(DungeonMap map, Point location) : base(map, location) { }
-        public override char Visual { get; set; } = '%';
+        public override char Visual { get; set; } = '-';
         public int TrapDamage { get; private set; }
 
         public Trap(int trapDamage, DungeonMap map, Point location) : base(map, location)
